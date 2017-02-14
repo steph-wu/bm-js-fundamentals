2a146e8893d7fd97ba26191e7ce8eabeb8ad3e02
STEPHANIE <WU.STEPHANIE@LIVE.COM>
2017-02-14 11:57:00 -0500

    Create pacman-arrays.js and print names of ghosts in for loop

---
+++ pacman-arrays.js
@@ -0,0 +1,6 @@
+var ghosts = ['Inky', 'Blinky', 'Pinky', 'Clyde']
+console.log('There are ' + ghosts.length + ' ghosts.')
+console.log('Their names are: ')
+for (var i = 0; i < ghosts.length; i++) {
+  console.log(ghosts[i]);
+};
