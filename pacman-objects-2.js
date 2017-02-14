07113f531d9f5f2d01e875f76c7140136e3504a5
STEPHANIE <WU.STEPHANIE@LIVE.COM>
2017-02-14 12:02:52 -0500

    Create javascript object files

---
+++ pacman-objects-2.js
@@ -0,0 +1,8 @@
+var ghosts = {inky: 'Cyan', blinky: 'Red', pinky: 'Pink', clyde: 'Orange'};
+console.log(ghosts);
+
+console.log(ghosts.inky);
+console.log(ghosts['inky']);
+
+var ghostName = 'clyde';
+console.log(ghosts[ghostName]);
